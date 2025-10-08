5ff3786269011e30f5c347a6f150ad9f99839753
Sowmiya Palanisamy <psowmiya2504@gmail.com>
2025-10-08 10:35:54 +0530

    Autobiographical_number.java

--- Autobiographical_number.java
+++ Autobiographical_number.java
@@ -1,7 +1,15 @@
-// reads a single string input (expected to be a numeric string) and checks if the string represents a 
-// self-describing number under certain constraints. If the string meets all the criteria, it outputs 
-// the number of unique digits in the string; otherwise, it outputs -1.
+/*   Reads a single string input (expected to be a numeric string) and checks if the string represents a 
+self-describing number under certain constraints. If the string meets all the criteria, it outputs 
+the number of unique digits in the string; otherwise, it outputs -1.
 
+    i/p:1210     o/p: 3
+    i/p:1234     o/p: -1  (sum of digit != no of digits)
+Rule 1: String length must be within 10,
+Rule 2: Length of the string == sum of the digits
+Rule 3: If length is 4 , count 0,1,2,3s' count to make as string, then check equals to input stirng
+        count(0)=1,count(1)=2,count(2)=1,count(3)=0 --> 1210 which is equal to i/p string
+Rule 4: If the above conditions get pass, then return no.of unique digits in the given string(3),
+        Otherwise return -1.     */
 
 
 
